fc99479810997849d617e19a67530a4d8d1fd502
louiseBEROS23 <louise.beros.beros@gmail.com>
2024-07-22 12:12:49 +0200

    Update gwz_sales.sql

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,5 +1,6 @@
 SELECT 
   date_date
   ,ROUND(SUM(turnover),2) as total_turnover
+  ,ROUND(SUM(purchase_cost),2) as total_purchase_cost
  FROM `data-analytics-bootcamp-363212.course14.gwz_sales` 
 GROUP BY date_date
